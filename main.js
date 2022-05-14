c1f159ef72c9a6beb57c8fe57cbfe0398b03964a
Warley Afonso <warleysilva365@gmail.com>
2022-05-13 21:01:41 -0300

    Inserindo a o fetch API

--- main.js
+++ main.js
@@ -7,10 +7,21 @@ import {
 const title = document.getElementById('title')
 const description = document.getElementById('description');
 const button = document.querySelector('button')
-//const movieApi = `${BASE_URL}${idMovie}${API_KEY}${language}`
+const image = document.querySelector('#image')
 
-button.addEventListener('click', idMovi => {
-  const idMovie =  Math.floor(Math.random() * (100000 - 10)) + 10
-  console.log(idMovie)
-})
 
+async function filme() {
+  const movie =  Math.floor(Math.random() * (100000 - 10)) + 10
+  const url = `${BASE_URL}${movie}${API_KEY}`;
+  const infoFilme = await fetch(url).then(r => r.json());
+  const img = `${IMG_URL}${infoFilme.poster_path}`
+  
+  title.innerHTML = infoFilme.title;
+  description.innerHTML = infoFilme.overview;
+  image.setAttribute('src', img)
+
+  console.log(infoFilme)
+}
+
+
+button.addEventListener('click', filme);
