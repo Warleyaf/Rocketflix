8e8603bfb558c420654a6ab2d2dd7f0ec1dba1db
Warley Afonso <warleysilva365@gmail.com>
2022-05-14 16:15:07 -0300

    Adicionando responsividade

--- main.js
+++ main.js
@@ -14,15 +14,16 @@ button.addEventListener('click', async() => {
     const data = await fetch(url).then(response => response.json());
     infoMovie.innerHTML = `
       <img src="${IMG_URL}${data.poster_path}"/>
-      <div>
+      <div class="movie-data">
         <h1> ${data.title} </h1>
         <p> ${data.overview} </p>
       </div>
     `
+
     if (data.success === false) {
       infoMovie.innerHTML = `
       <img src="./assets/programing-study.gif"/>
-      <div>
+      <div class="movie-data">
         <h1> Filme não encontrado </h1>
         <p> Isso é um sinal pra você estudar 📖, então vamos!!!, até por que seu futuro só depende de você. Lembre-se, tu não nasceu herdeiro é triste... eu sei mas boraa. </p>
       </div>
